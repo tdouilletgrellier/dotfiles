ab3919b95d5918db57e7a1568243f2ac94e36a06
Thomas Douillet-Grellier <thomas.douillet-grellier@edf.fr>
2024-05-21 11:52:24 +0200

    update extremeultimatbashrc

--- config/ExtremeUltimateBashrc/update.sh
+++ config/ExtremeUltimateBashrc/update.sh
@@ -1,3 +1,28 @@
-wget -O ~/dotfiles/config/ExtremeUltimateBashrc/.bashrc https://sourceforge.net/projects/ultimate-bashrc/files/_bashrc/download
-wget -O ~/dotfiles/config/ExtremeUltimateBashrc/.bashrc_help https://sourceforge.net/projects/ultimate-bashrc/files/_bashrc_help/download
-wget -O ~/dotfiles/config/ExtremeUltimateBashrc/.bashrc_prompt https://sourceforge.net/projects/ultimate-bashrc/files/_bashrc_prompt/download
+curl -L --output ./_bashrc https://sourceforge.net/projects/ultimate-bashrc/files/_bashrc/download
+curl -L --output ./_bashrc_help https://sourceforge.net/projects/ultimate-bashrc/files/_bashrc_help/download
+curl -L --output ./_bashrc_prompt https://sourceforge.net/projects/ultimate-bashrc/files/_bashrc_prompt/download
+#
+cd support/
+curl -L --output ./alacritty.toml https://sourceforge.net/projects/ultimate-bashrc/files/support/alacritty.toml/download
+curl -L --output ./fonts.conf https://sourceforge.net/projects/ultimate-bashrc/files/support/fonts.conf/download
+curl -L --output ./_inputrc https://sourceforge.net/projects/ultimate-bashrc/files/support/_inputrc/download
+curl -L --output ./_nanorc https://sourceforge.net/projects/ultimate-bashrc/files/support/_nanorc/download
+curl -L --output ./_screenrc https://sourceforge.net/projects/ultimate-bashrc/files/support/_screenrc/download
+curl -L --output ./_tmux.conf https://sourceforge.net/projects/ultimate-bashrc/files/support/_tmux.conf/download
+curl -L --output ./upd https://sourceforge.net/projects/ultimate-bashrc/files/support/upd/download
+curl -L --output ./_vimrc https://sourceforge.net/projects/ultimate-bashrc/files/support/_vimrc/download
+curl -L --output ./_wezterm.lua https://sourceforge.net/projects/ultimate-bashrc/files/support/_wezterm.lua/download
+curl -L --output ./_Xmodmap https://sourceforge.net/projects/ultimate-bashrc/files/support/_Xmodmap/download
+curl -L --output ./_xprofile https://sourceforge.net/projects/ultimate-bashrc/files/support/_xprofile/download
+curl -L --output ./_Xresources https://sourceforge.net/projects/ultimate-bashrc/files/support/_Xresources/download
+curl -L --output ./_zshrc https://sourceforge.net/projects/ultimate-bashrc/files/support/_zshrc/download
+cd ..
+#
+cd bashrc.d/
+curl -L --output ./arch_mirrors_support https://sourceforge.net/projects/ultimate-bashrc/files/bashrc.d/arch_mirrors_support/download
+curl -L --output ./clear_color_spark https://sourceforge.net/projects/ultimate-bashrc/files/bashrc.d/clear_color_spark/download
+curl -L --output ./trueline_config https://sourceforge.net/projects/ultimate-bashrc/files/bashrc.d/trueline_config/download
+curl -L --output ./trueline_def https://sourceforge.net/projects/ultimate-bashrc/files/bashrc.d/trueline_def/download
+curl -L --output ./tty_terminal_color_scheme https://sourceforge.net/projects/ultimate-bashrc/files/bashrc.d/tty_terminal_color_scheme/download
+cd ..
+
