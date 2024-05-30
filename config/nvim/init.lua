170840e25586438547870ad9310d6a9d8d852743
Thomas Douillet-Grellier <thomas.douillet-grellier@edf.fr>
2024-05-30 13:37:03 +0200

    config

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,4 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
-vim.cmd("colorscheme cyberdream") -- if the above fails, then use default
+vim.cmd("colorscheme cyberdream") -- catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
+-- vim.cmd("colorscheme catppuccin") -- catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
