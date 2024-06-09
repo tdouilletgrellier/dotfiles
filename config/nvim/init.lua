e4f505d4012e344905fd093344ec9733a4a910a8
Thomas Douillet-Grellier <thomas.douillet-grellier@edf.fr>
2024-06-09 21:01:05 +0200

    epx vim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -3,3 +3,16 @@ require("config.lazy")
 
 vim.cmd("colorscheme cyberdream") -- catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
 -- vim.cmd("colorscheme catppuccin") -- catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
+
+vim.cmd("highlight epxNumber      ctermfg=red  guifg=#cd0000")
+vim.cmd("highlight epxWord        ctermfg=white  guifg=#ffffff")
+vim.cmd("highlight epxKeywords    ctermfg=blue  guifg=#386bd7")
+vim.cmd("highlight epxKeywords0   ctermfg=green  guifg=#98d62d")
+vim.cmd("highlight epxVariable    ctermfg=magenta  guifg=#db67e6")
+vim.cmd("highlight epxEqual       ctermfg=green  guifg=#79ff0f")
+vim.cmd("highlight epxString      ctermfg=yellow  guifg=#f3d64e")
+vim.cmd("highlight epxComment     ctermfg=grey  guifg=#666666")
+vim.cmd("highlight epxTitle       ctermfg=yellow  guifg=#ffbd5e")
+vim.cmd("highlight epxDescBlock   ctermfg=grey  guifg=#666666")
+
+vim.cmd("au BufRead,BufNewFile *.epx set filetype=epx")
