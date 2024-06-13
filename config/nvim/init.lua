f5fef81bf82fb7ab6846b33ee8988de7c671ed38
Thomas Douillet-Grellier <thomas.douillet-grellier@edf.fr>
2024-06-13 10:59:41 +0200

    config

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,5 +1,9 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+local cyberdream = require("lualine.themes.cyberdream").get_theme()
+require('lualine').setup {
+  options = { theme  = cyberdream },
+}
 
 vim.cmd("colorscheme cyberdream") -- catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
 -- vim.cmd("colorscheme catppuccin") -- catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
@@ -19,74 +23,121 @@ vim.cmd("au BufRead,BufNewFile *.epx set filetype=epx")
 
 vim.cmd("let b:fortran_fixed_source = 1")
 
--- Mappings
 
--- <C> -> Ctrl
--- <leader> -> Space
--- <A> -> alt
--- <S> -> shift
--- <M> -> meta (cmd key on mac)
--- <D> -> super (windows key on windows)
--- <kPoint> -> Keypad Point (.)
--- <kEqual> -> Keypad Equal (=)
--- <kPlus> -> Keypad Plus (+)
--- <kMinus> -> Keypad Minus (-)
+local hardmode = true
+if hardmode then
+    -- Show an error message if a disabled key is pressed
+    local msg = [[<cmd>echohl Error | echo "KEY DISABLED" | echohl None<CR>]]
 
---[[vim.keymap.set("n", "<A-/>", function()
-  require("Comment.api").toggle.linewise.current()
-end, { desc = "  Toggle comment"})]]
+    -- Disable arrow keys in insert mode with a styled message
+    vim.api.nvim_set_keymap('i', '<Up>', '<C-o>' .. msg, { noremap = true, silent = false })
+    vim.api.nvim_set_keymap('i', '<Down>', '<C-o>' .. msg, { noremap = true, silent = false })
+    vim.api.nvim_set_keymap('i', '<Left>', '<C-o>' .. msg, { noremap = true, silent = false })
+    vim.api.nvim_set_keymap('i', '<Right>', '<C-o>' .. msg, { noremap = true, silent = false })
+    vim.api.nvim_set_keymap('i', '<Del>', '<C-o>' .. msg, { noremap = true, silent = false })
+    vim.api.nvim_set_keymap('i', '<BS>', '<C-o>' .. msg, { noremap = true, silent = false })
 
--- NewLine with Shift+Enter
-vim.keymap.set("n", "o", "o<ESC>", { desc = " New line Below" })
-vim.keymap.set("n", "O", "O<ESC>", { desc = " New line Above" })
+    -- Disable arrow keys in normal mode with a styled message
+    vim.api.nvim_set_keymap('n', '<Up>', msg, { noremap = true, silent = false })
+    vim.api.nvim_set_keymap('n', '<Down>', msg, { noremap = true, silent = false })
+    vim.api.nvim_set_keymap('n', '<Left>', msg, { noremap = true, silent = false })
+    vim.api.nvim_set_keymap('n', '<Right>', msg, { noremap = true, silent = false })
+    vim.api.nvim_set_keymap('n', '<BS>', msg, { noremap = true, silent = false })
+end
+
+vim.cmd("nmap oo o<Esc>")
+vim.cmd("nmap OO O<Esc>")
 
 
--- Move line up and down
-vim.keymap.set("i", "<A-Up>", "<CMD>m .-2<CR>==", { desc = "󰜸 Move line up" })
-vim.keymap.set("i", "<A-Down>", "<CMD>m .+1<CR>==", { desc = "󰜸 Move line down" })
+-- vim.cmd("nmap <CR> :a<CR><CR>.<CR>")
 
--- Navigate (insert)
-vim.keymap.set("i", "<A-Left>", "<ESC>I", { desc = " Move to beginning of line" })
-vim.keymap.set("i", "<A-Right>", "<ESC>A", { desc = " Move to end of line" })
--- vim.keymap.set("i", "<A-d>", "<ESC>diw", { desc = " Delete word" })
+-- vim.keymap.set("n", "<CR>", "mao<esc>0<S-d>`a<cmd>delmarks a<cr>", { desc = "Add new line below" })
+-- vim.keymap.set("n", "<S-CR>", "maO<esc>0<S-d>`a<cmd>delmarks a<cr>", { desc = "Add new line above" })
 
-vim.keymap.set("n", "<A-Left>", "<ESC>_", { desc = "󰜲 Move to beginning of line" })
-vim.keymap.set("n", "<A-Right>", "<ESC>$", { desc = "󰜵 Move to end of line" })
-vim.keymap.set("n", "<C-a>", "gg0vG", { desc = " Select all" })
--- vim.keymap.set("n", "<F3>", "nzzzv", { desc = " Next" })
--- vim.keymap.set("n", "<S-F3>", "Nzzzv", { desc = " Previous" })
-vim.keymap.set("n", "<C-z>", "<ESC><CMD>u<CR>", { desc = "󰕌 Undo" })
-vim.keymap.set("n", "<C-r>", "<ESC><CMD>redo<CR>", { desc = "󰑎 Redo" })
-vim.keymap.set("v", "<C-z>", "<ESC><CMD>u<CR>", { desc = "󰕌 Undo" })
-vim.keymap.set("v", "<C-r>", "<ESC><CMD>redo<CR>", { desc = "󰑎 Redo" })
-vim.keymap.set("i", "<C-z>", "<ESC><CMD>u<CR>", { desc = "󰕌 Undo" })
-vim.keymap.set("i", "<C-r>", "<ESC><CMD>redo<CR>", { desc = "󰑎 Redo" })
-vim.keymap.set("n", "<BS>", "<C-o>", { desc = "Return" })
+-- -- Set keymap for normal windows only (e.g. not quickfix)
+-- local normal_window_keymap = function(mode, lhs, rhs, opts)
+--   local merged_opts = vim.tbl_extend("force", { noremap = true, expr = true }, opts or {})
+-- 
+--   vim.keymap.set(mode, lhs, function()
+--     local buftype = vim.fn.win_gettype()
+--     return buftype == "" and rhs or lhs
+--   end, merged_opts)
+-- end
+-- 
+-- -- Open new line (like o/O) without moving the cursor, without entering insert mode and removing any characters
+-- normal_window_keymap("n", "<CR>", "mao<esc>0<S-d>`a<cmd>delmarks a<cr>", { desc = "Add new line below" })
+-- normal_window_keymap("n", "<S-CR>", "maO<esc>0<S-d>`a<cmd>delmarks a<cr>", { desc = "Add new line above" })
 
--- vim.keymap.set("n", "<C-x>", "x", { desc = "󰆐 Cut" })
--- vim.keymap.set("n", "<C-v>", "p`[v`]=", { desc = "󰆒 Paste" })
--- vim.keymap.set("n", "<C-c>", "y", { desc = " Copy" })
 
--- vim.keymap.set("i", "<C-c>", "y", { desc = " Copy" })
--- vim.keymap.set("v", "<C-c>", "y", { desc = " Copy" })
 
-vim.keymap.set("n", "p", "p`[v`]=", { desc = "󰆒 Paste" })
-vim.keymap.set("n", "<leader><leader>p","printf('`[%s`]', getregtype()[0])", { desc = "Reselect last pasted area" }, {expr = true})
-vim.keymap.set("n", "<leader><leader>d", "viw",{ desc = " Select word" })
-vim.keymap.set("n", "<leader>d", 'viw"_di', { desc = " Delete word" })
-vim.keymap.set("n", "<A-Up>", "<CMD>m .-2<CR>==", { desc = "󰜸 Move line up" })
-vim.keymap.set("n", "<A-Down>", "<CMD>m .+1<CR>==", { desc = "󰜯 Move line down" })        
-
-vim.keymap.set("v", "<A-Up>", ":m'<-2<CR>gv=gv", { desc = "󰜸 Move selection up"}, {opts = { silent = true } })
-vim.keymap.set("v", "<A-Down>", ":m'>+1<CR>gv=gv", { desc = "󰜯 Move selection down"}, {opts = { silent = true } })
---vim.keymap.set("v", "<Home>", "gg", { desc = "Home" })
---vim.keymap.set("v", "<End>", "G", { desc = "End" })
-vim.keymap.set("v", "y", "y`]", { desc = "Yank and move to end" })
-vim.keymap.set("v", "<", "<gv", { desc = " Ident backward"}, {opts = { silent = false }})
-vim.keymap.set("v", ">", ">gv", { desc = " Ident forward"}, {opts = { silent = false }})
- 
-vim.keymap.set("v", "<A-Left>", "<ESC>_", { desc = "󰜲 Move to beginning of line" })
-vim.keymap.set("v", "<A-Right>", "<ESC>$", { desc = "󰜵 Move to end of line" })
-
-vim.keymap.set("n", "<leader><leader>h", "<CMD>vs <CR>", { desc = "󰤼 Vertical split"}, {opts = { nowait = true } })
-vim.keymap.set("n", "<leader><leader>v", "<CMD>sp <CR>", { desc = "󰤻 Horizontal split"}, {opts = { nowait = true } })
+-- -- Mappings
+-- 
+-- -- <C> -> Ctrl
+-- -- <leader> -> Space
+-- -- <A> -> alt
+-- -- <S> -> shift
+-- -- <M> -> meta (cmd key on mac)
+-- -- <D> -> super (windows key on windows)
+-- -- <kPoint> -> Keypad Point (.)
+-- -- <kEqual> -> Keypad Equal (=)
+-- -- <kPlus> -> Keypad Plus (+)
+-- -- <kMinus> -> Keypad Minus (-)
+-- 
+-- --[[vim.keymap.set("n", "<A-/>", function()
+--   require("Comment.api").toggle.linewise.current()
+-- end, { desc = "  Toggle comment"})]]
+-- 
+-- -- NewLine with Shift+Enter
+-- vim.keymap.set("n", "o", "o<ESC>", { desc = " New line Below" })
+-- vim.keymap.set("n", "O", "O<ESC>", { desc = " New line Above" })
+-- 
+-- 
+-- -- Move line up and down
+-- vim.keymap.set("i", "<A-Up>", "<CMD>m .-2<CR>==", { desc = "󰜸 Move line up" })
+-- vim.keymap.set("i", "<A-Down>", "<CMD>m .+1<CR>==", { desc = "󰜸 Move line down" })
+-- 
+-- -- Navigate (insert)
+-- vim.keymap.set("i", "<A-Left>", "<ESC>I", { desc = " Move to beginning of line" })
+-- vim.keymap.set("i", "<A-Right>", "<ESC>A", { desc = " Move to end of line" })
+-- -- vim.keymap.set("i", "<A-d>", "<ESC>diw", { desc = " Delete word" })
+-- 
+-- vim.keymap.set("n", "<A-Left>", "<ESC>_", { desc = "󰜲 Move to beginning of line" })
+-- vim.keymap.set("n", "<A-Right>", "<ESC>$", { desc = "󰜵 Move to end of line" })
+-- vim.keymap.set("n", "<C-a>", "gg0vG", { desc = " Select all" })
+-- -- vim.keymap.set("n", "<F3>", "nzzzv", { desc = " Next" })
+-- -- vim.keymap.set("n", "<S-F3>", "Nzzzv", { desc = " Previous" })
+-- vim.keymap.set("n", "<C-z>", "<ESC><CMD>u<CR>", { desc = "󰕌 Undo" })
+-- vim.keymap.set("n", "<C-r>", "<ESC><CMD>redo<CR>", { desc = "󰑎 Redo" })
+-- vim.keymap.set("v", "<C-z>", "<ESC><CMD>u<CR>", { desc = "󰕌 Undo" })
+-- vim.keymap.set("v", "<C-r>", "<ESC><CMD>redo<CR>", { desc = "󰑎 Redo" })
+-- vim.keymap.set("i", "<C-z>", "<ESC><CMD>u<CR>", { desc = "󰕌 Undo" })
+-- vim.keymap.set("i", "<C-r>", "<ESC><CMD>redo<CR>", { desc = "󰑎 Redo" })
+-- vim.keymap.set("n", "<BS>", "<C-o>", { desc = "Return" })
+-- 
+-- -- vim.keymap.set("n", "<C-x>", "x", { desc = "󰆐 Cut" })
+-- -- vim.keymap.set("n", "<C-v>", "p`[v`]=", { desc = "󰆒 Paste" })
+-- -- vim.keymap.set("n", "<C-c>", "y", { desc = " Copy" })
+-- 
+-- -- vim.keymap.set("i", "<C-c>", "y", { desc = " Copy" })
+-- -- vim.keymap.set("v", "<C-c>", "y", { desc = " Copy" })
+-- 
+-- vim.keymap.set("n", "p", "p`[v`]=", { desc = "󰆒 Paste" })
+-- vim.keymap.set("n", "<leader><leader>p","printf('`[%s`]', getregtype()[0])", { desc = "Reselect last pasted area" }, {expr = true})
+-- vim.keymap.set("n", "<leader><leader>d", "viw",{ desc = " Select word" })
+-- vim.keymap.set("n", "<leader>d", 'viw"_di', { desc = " Delete word" })
+-- vim.keymap.set("n", "<A-Up>", "<CMD>m .-2<CR>==", { desc = "󰜸 Move line up" })
+-- vim.keymap.set("n", "<A-Down>", "<CMD>m .+1<CR>==", { desc = "󰜯 Move line down" })        
+-- 
+-- vim.keymap.set("v", "<A-Up>", ":m'<-2<CR>gv=gv", { desc = "󰜸 Move selection up"}, {opts = { silent = true } })
+-- vim.keymap.set("v", "<A-Down>", ":m'>+1<CR>gv=gv", { desc = "󰜯 Move selection down"}, {opts = { silent = true } })
+-- --vim.keymap.set("v", "<Home>", "gg", { desc = "Home" })
+-- --vim.keymap.set("v", "<End>", "G", { desc = "End" })
+-- vim.keymap.set("v", "y", "y`]", { desc = "Yank and move to end" })
+-- vim.keymap.set("v", "<", "<gv", { desc = " Ident backward"}, {opts = { silent = false }})
+-- vim.keymap.set("v", ">", ">gv", { desc = " Ident forward"}, {opts = { silent = false }})
+--  
+-- vim.keymap.set("v", "<A-Left>", "<ESC>_", { desc = "󰜲 Move to beginning of line" })
+-- vim.keymap.set("v", "<A-Right>", "<ESC>$", { desc = "󰜵 Move to end of line" })
+-- 
+-- vim.keymap.set("n", "<leader><leader>h", "<CMD>vs <CR>", { desc = "󰤼 Vertical split"}, {opts = { nowait = true } })
+-- vim.keymap.set("n", "<leader><leader>v", "<CMD>sp <CR>", { desc = "󰤻 Horizontal split"}, {opts = { nowait = true } })-- 
--- config/vimrc/my_configs.vim
+++ config/vimrc/my_configs.vim
@@ -21,3 +21,24 @@ highlight epxString      ctermfg=yellow  guifg=#f3d64e
 highlight epxComment     ctermfg=grey  guifg=#666666
 highlight epxTitle       ctermfg=yellow  guifg=#ffbd5e
 highlight epxDescBlock   ctermfg=grey  guifg=#666666
+
+
+nnoremap <Left>  :echoe "Use h"<CR>
+nnoremap <Right> :echoe "Use l"<CR>
+nnoremap <Up>    :echoe "Use k"<CR>
+nnoremap <Down>  :echoe "Use j"<CR>
+" ...and in insert mode
+inoremap <Left>  <ESC>:echoe "Use h"<CR>
+inoremap <Right> <ESC>:echoe "Use l"<CR>
+inoremap <Up>    <ESC>:echoe "Use k"<CR>
+inoremap <Down>  <ESC>:echoe "Use j"<CR>
+" ...and in command mode
+cnoremap <Left>  <ESC>:echoe "Use h"<CR>
+cnoremap <Right> <ESC>:echoe "Use l"<CR>
+cnoremap <Up>    <ESC>:echoe "Use k"<CR>
+cnoremap <Down>  <ESC>:echoe "Use j"<CR>
+" ...and in visual mode
+vnoremap <Left>  <ESC>:echoe "Use h"<CR>
+vnoremap <Right> <ESC>:echoe "Use l"<CR>
+vnoremap <Up>    <ESC>:echoe "Use k"<CR>
+vnoremap <Down>  <ESC>:echoe "Use j"<CR>
