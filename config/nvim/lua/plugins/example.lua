b7b44654c0783934c9613cdf6375de7bf63a1aed
Thomas Douillet-Grellier <thomas.douillet-grellier@edf.fr>
2024-06-14 09:36:51 +0200

    config

--- config/nvim/lua/plugins/example.lua
+++ config/nvim/lua/plugins/example.lua
@@ -1,6 +1,15 @@
 return {
 
   {
+    "nvim-neo-tree/neo-tree.nvim",
+    config = function()
+      require("neo-tree").setup({
+        close_if_last_window = true,
+      })
+    end,
+  },
+
+  {
     "mcauley-penney/visual-whitespace.nvim",
     event = "ModeChanged",
     opts = {
@@ -172,50 +181,50 @@ return {
 
       -- Turn off VimTeX indentation
       vim.g.vimtex_indent_enabled = 0
-      
+
       -- Disable default mappings; I'll define my own
       vim.g.vimtex_mappings_enabled = 0
-      
+
       -- Disable insert mode mappings (in favor of a dedicated snippet engine)
       vim.g.vimtex_imaps_enabled = 0
-      
+
       -- Disable completion
       -- vim.g.vimtex_complete_enabled = 0
-      
+
       -- Disable syntax conceal
-      vim.g.vimtex_syntax_conceal_disable = 1  
-      
+      vim.g.vimtex_syntax_conceal_disable = 1
+
       -- Default is 500 lines and gave me lags on missed key presses
       vim.g.vimtex_delim_stopline = 50
-      
+
       -- VimTeX toggle delimeter configuration
       -- vim.g.vimtex_delim_toggle_mod_list = { ['\left', '\right'] , ['\big', '\big'], }
-      
+
       -- Don't open quickfix for warning messages if no errors are present
-      vim.g.vimtex_quickfix_open_on_warning = 0  
-      
+      vim.g.vimtex_quickfix_open_on_warning = 0
+
       -- Disable some compilation warning messages
       vim.g.vimtex_quickfix_ignore_filters = {
-             'LaTeX hooks Warning',
-             'Underfull \\hbox',
-             'Overfull \\hbox',
-            -- 'LaTeX Warning: .\+ float specifier changed to',
-             'Package siunitx Warning: Detected the "physics-- package:',
-             'Package hyperref Warning: Token not allowed in a PDF string',
-             'Fatal error occurred, no output PDF file produced!',
-            }
-      
+        "LaTeX hooks Warning",
+        "Underfull \\hbox",
+        "Overfull \\hbox",
+        -- 'LaTeX Warning: .\+ float specifier changed to',
+        'Package siunitx Warning: Detected the "physics-- package:',
+        "Package hyperref Warning: Token not allowed in a PDF string",
+        "Fatal error occurred, no output PDF file produced!",
+      }
+
       -- if g:os_current == "Linux"
-      vim.g.vimtex_view_method = 'zathura'
+      vim.g.vimtex_view_method = "zathura"
       -- elseif g:os_current == "Darwin"
       --   vim.g.vimtex_view_method = 'skim'
       -- else
       --   echom "Error: LaTeX forward show not supported on this OS"
       -- endif
-      
+
       -- Don't automatically open PDF viewer after first compilation
       vim.g.vimtex_view_automatic = 0
-      
+
       -- Disable continuous compilation
       --vim.g.vimtex_compiler_latexmk = {
       --     'build_dir' = '',
@@ -230,7 +239,6 @@ return {
       --       '-interaction=nonstopmode',
       --     ],
       --    }
-
     end,
   },
 
