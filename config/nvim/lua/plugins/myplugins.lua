36d4606c16f846b88e858a6e3dfef315cd620053
Thomas Douillet-Grellier <thomas.douillet-grellier@edf.fr>
2024-11-07 09:26:50 +0100

    nvim disabled autocomplete

--- config/nvim/lua/plugins/myplugins.lua
+++ config/nvim/lua/plugins/myplugins.lua
@@ -58,6 +58,41 @@ return {
     end,
   },
 
+{
+    "hrsh7th/nvim-cmp",
+    init = function()
+      vim.g.cmp_disabled = false
+    end,
+    opts = function(_, opts)
+      opts.enabled = function()
+        -- local context = require("cmp.config.context")
+        if vim.g.cmp_disabled == true then
+          return false
+        end
+        -- some other conditions (like not in commments) can go here
+        return not disabled
+      end
+    end,
+    keys = {
+      {
+        "<leader>ua",
+        function()
+          vim.g.cmp_disabled = not vim.g.cmp_disabled
+          local msg = ""
+          if vim.g.cmp_disabled == true then
+            msg = "Autocompletion (cmp) disabled"
+          else
+            msg = "Autocompletion (cmp) enabled"
+          end
+          vim.notify(msg, vim.log.levels.INFO)
+        end,
+        noremap = true,
+        silent = true,
+        desc = "toggle autocompletion",
+      },
+    },
+  },
+
   {
     "kdheepak/lazygit.nvim",
     cmd = {
