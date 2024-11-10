56f09e6accd1ef2cd6a3712c9050c42bd9f38154
Thomas Douillet-Grellier <thomas.douillet-grellier@edf.fr>
2024-11-10 23:28:21 +0100

    nvim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,20 +1,26 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Cyberdream lualine theme
 local cyberdream = require("lualine.themes.cyberdream") --.get_theme()
 require("lualine").setup({
   options = { theme = cyberdream },
 })
 vim.cmd("colorscheme cyberdream")
+
+-- Vim theme (green on black)
 -- vim.cmd("colorscheme vim")
 
+-- Telescope transparency
 vim.cmd("highlight TelescopeBorder guibg=none")
 vim.cmd("highlight TelescopeTitle guibg=none")
-
 vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
 
+-- No cursor line, fomrating current line number
 vim.o.cursorline = true -- does not enable cursor line
 vim.o.cursorlineopt = number -- does not enable cursor line
 vim.cmd("highlight CursorLine cterm=NONE ctermbg=NONE ctermfg=NONE guibg=NONE guifg=NON")
 vim.cmd("highlight CursorLineNr cterm=bold term=bold gui=bold")
 
+-- Hardtime on
 require("hardtime").setup()
