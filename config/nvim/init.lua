a7b80b0977b8148c983c14bff1ab7c2216bddd83
Thomas Douillet-Grellier <thomas.douillet-grellier@edf.fr>
2024-12-02 11:43:25 +0100

    vim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -27,33 +27,33 @@ vim.cmd("highlight CursorLine guibg=#2a2a2a")
 vim.cmd("highlight CursorColumn guibg=#2a2a2a")
 
 -- Mouse on
-vim.o.mouse = "a" 
+vim.o.mouse = "a"
 
 -- Hardtime on
-require("hardtime").setup()
+-- require("hardtime").setup()
 local highlight = {
-    "RainbowRed",
-    "RainbowYellow",
-    "RainbowBlue",
-    "RainbowOrange",
-    "RainbowGreen",
-    "RainbowViolet",
-    "RainbowCyan",
+  "RainbowRed",
+  "RainbowYellow",
+  "RainbowBlue",
+  "RainbowOrange",
+  "RainbowGreen",
+  "RainbowViolet",
+  "RainbowCyan",
 }
-local hooks = require "ibl.hooks"
+local hooks = require("ibl.hooks")
 -- create the highlight groups in the highlight setup hook, so they are reset
 -- every time the colorscheme changes
 hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-    vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#cd0000" })
-    vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#e7bf00" })
-    vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#386bd7" })
-    vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#ff8700" })
-    vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#79ff0f" })
-    vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#db67e6" })
-    vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#79dff2" })
+  vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#cd0000" })
+  vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#e7bf00" })
+  vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#386bd7" })
+  vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#ff8700" })
+  vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#79ff0f" })
+  vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#db67e6" })
+  vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#79dff2" })
 end)
 
 vim.g.rainbow_delimiters = { highlight = highlight }
-require("ibl").setup { scope = { highlight = highlight } }
+require("ibl").setup({ scope = { highlight = highlight } })
 
 hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
