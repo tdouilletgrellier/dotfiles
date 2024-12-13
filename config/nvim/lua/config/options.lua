174c043684410f8affddc8ba8ce4fa0915833399
Thomas Douillet-Grellier <thomas.douillet-grellier@edf.fr>
2024-12-13 13:17:21 +0100

    update

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -9,23 +9,22 @@ local palette = require("colors.palette_custom")
 
 -- Define the highlights using the custom palette
 local highlights = {
-  epxNumber    = { fg = palette.red },          -- Use 'red' from custom palette
-  epxWord      = { fg = palette.fg },           -- Use 'fg' (foreground) for general words
-  epxKeywords  = { fg = palette.blue },         -- Use 'blue' for primary keywords
+  epxNumber = { fg = palette.red }, -- Use 'red' from custom palette
+  epxWord = { fg = palette.fg }, -- Use 'fg' (foreground) for general words
+  epxKeywords = { fg = palette.blue }, -- Use 'blue' for primary keywords
   epxKeywords0 = { fg = palette.green }, -- Use 'successGreen' for alternate keywords
-  epxVariable  = { fg = palette.magenta },      -- Use 'magenta' for variables
-  epxEqual     = { fg = palette.green },        -- Use 'green' for equality operators
-  epxString    = { fg = palette.yellow },   -- Use 'neonYellow' for strings
-  epxComment   = { fg = palette.bgHighlight },  -- Use 'bgHighlight' for comments
-  epxTitle     = { fg = palette.orange },       -- Use 'orange' for titles
-  epxDescBlock = { fg = palette.grey },         -- Use 'grey' for descriptive blocks
+  epxVariable = { fg = palette.magenta }, -- Use 'magenta' for variables
+  epxEqual = { fg = palette.green }, -- Use 'green' for equality operators
+  epxString = { fg = palette.yellow }, -- Use 'neonYellow' for strings
+  epxComment = { fg = palette.bgHighlight }, -- Use 'bgHighlight' for comments
+  epxTitle = { fg = palette.orange }, -- Use 'orange' for titles
+  epxDescBlock = { fg = palette.grey }, -- Use 'grey' for descriptive blocks
 }
 
 -- Apply the highlights
 for group, style in pairs(highlights) do
   vim.api.nvim_set_hl(0, group, style)
 end
-
 
 for group, settings in pairs(highlights) do
   vim.api.nvim_set_hl(0, group, settings)
