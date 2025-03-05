97ce8a57bf9aeebae918f0c490e7e29b733f68ec
Thomas Douillet-Grellier <thomas.douillet-grellier@edf.fr>
2025-03-05 13:20:48 +0100

    radio

--- utils/radio.sh
+++ utils/radio.sh
@@ -1,2 +1,36 @@
-alias kiss='mplayer http://oom-cmg.streamguys1.com/atl1041/atl1041.mp3'
-#Kiss 104.1-WALR-FM-FM 104.1-Atlanta,GA :SOURCE:view-source:http://streema.com/radios/play/3806
+# Function to choose and play radio stations using ffplay
+play_radio() {
+  echo "Select a radio station to play:"
+  echo "1) Radio Mille Patte"
+  echo "2) FIP Reggae"
+  read -p "Enter the number of your choice: " choice
+  
+  case $choice in
+    1)
+      echo "Playing Radio Mille Patte..."
+      ffplay https://millepattes.ice.infomaniak.ch/millepattes-high.mp3 -nodisp -autoexit 
+      ;;
+    2)
+      echo "Playing FIP Reggae..."
+      ffplay http://direct.fipradio.fr/live/fip-webradio6.mp3 -nodisp -autoexit 
+      ;;
+    *)
+      echo "Invalid choice. Please select 1 or 2."
+      ;;
+  esac
+}
+
+# Detect if script is sourced or executed directly
+if [[ "${BASH_SOURCE[0]}" != "${0}" ]]; then
+    sourced=1
+else
+    sourced=0
+fi
+
+# Run or register aliases
+if [[ $sourced -eq 0 ]]; then
+    play_radio
+else
+    alias play-radio='play_radio'
+fi
+
