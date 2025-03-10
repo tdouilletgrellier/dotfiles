ba5b66e8353b2d2e8f32977568a1308878f16749
Thomas Douillet-Grellier <thomas.douillet-grellier@edf.fr>
2025-03-10 19:48:56 +0100

    feat : update_references_epx.sh

--- utils/update_references_epx.sh
+++ utils/update_references_epx.sh
@@ -11,6 +11,26 @@ process_logs() {
 	local verbose=0
 	local auto_confirm=0
 
+	# Foreground Colors
+	BLACK="\033[0;30m"
+	RED="\033[0;31m"
+	GREEN="\033[0;32m"
+	YELLOW="\033[0;33m"
+	BLUE="\033[0;34m"
+	MAGENTA="\033[0;35m"
+	CYAN="\033[0;36m"
+	WHITE="\033[0;37m"
+
+	# Bright Foreground Colors
+	BRIGHT_BLACK="\033[1;30m"
+	BRIGHT_RED="\033[1;31m"
+	BRIGHT_GREEN="\033[1;32m"
+	BRIGHT_YELLOW="\033[1;33m"
+	BRIGHT_BLUE="\033[1;34m"
+	BRIGHT_MAGENTA="\033[1;35m"
+	BRIGHT_CYAN="\033[1;36m"
+	BRIGHT_WHITE="\033[1;37m"
+
 	# Global configuration variables - users can modify these
 	# String patterns for matching
 	REF_PATTERN="REF="
@@ -250,7 +270,7 @@ process_logs() {
 # If it is sourced, the function will be available for later use.
 if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
 	if [[ $# -eq 0 ]]; then
-		show_help
+		process_logs
 	else
 		process_logs "$@"
 	fi
